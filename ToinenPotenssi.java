35a9acc5dd302395cee250e8091aaf5bd96bdafc
Iiro Pääkkönen <iiro.paakkonen@gmail.com>
2018-12-03 16:38:47 +0200

    Ohjelma laskee nyt myös muilla eksponenteilla

--- ToinenPotenssi.java
+++ ToinenPotenssi.java
@@ -1,11 +1,12 @@
 public class ToinenPotenssi{
   
   /**
-   *  Ohjelma korottaa parametrina saadun luvun toiseen potenssiin ja tulostaa sen
+   *  Ohjelma korottaa parametrina saadun luvun toisena parametrina saatuun eksponenttiin ja tulostaa sen
    */
   public static void main(String[] args){
    int luku = Integer.parseInt(args[0]);
-   double tulos = Math.pow(luku,2);
+   int eksponentti = Integer.parseInt(args[1]);
+   double tulos = Math.pow(luku,eksponentti);
    System.out.println(tulos);
   }
   
